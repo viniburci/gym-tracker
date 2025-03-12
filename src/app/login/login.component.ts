148d8b7bebcba6b366677b84481a957b8970ad3f
Vinicius Burci <vpburci@gmail.com>
2025-03-12 06:39:12 -0300

    LoadingSpinnerComponent

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../auth/auth.service';
+import { LoadingSpinner } from '../../public/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, LoadingSpinner],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
   private auth = inject(AuthService)
+  isLoading = signal(false);
 
   form = new FormGroup({
     email: new FormControl('', [Validators.email, Validators.required]),
@@ -38,7 +40,11 @@ export class LoginComponent {
     const email = this.form.controls.email.value;
     const password = this.form.controls.password.value
     console.log(email, password);
-    this.auth.signup(email!, password!).subscribe(response => console.log(response));
+    this.isLoading.set(true);
+    this.auth.signup(email!, password!).subscribe({
+      next: response => {console.log(response); this.isLoading.set(false)},
+      error: err => {console.log(err); this.isLoading.set(false)},
+    });
   }
 
 }
---
+++ src/public/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-loading-spinner',
+  template: '<span class="loader"></span>',
+  styleUrls: ['./loading-spinner.component.css'],
+  standalone: true
+})
+export class LoadingSpinner{}
