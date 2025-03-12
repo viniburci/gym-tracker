0bada87a3add04b47619d8084082ff007cf35704
Vinicius Burci <vpburci@gmail.com>
2025-03-12 05:42:12 -0300

    Signup Component

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+  })
+
+  get emailIsInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    )
+  }
+
+  get passwordIsInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    )
+  }
+
+  get repeatPasswordIsInvalid() {
+    return (
+      this.form.controls.confirmPassword.touched &&
+      this.form.controls.confirmPassword.dirty &&
+      this.form.controls.confirmPassword.value !== this.form.controls.password.value
+    )
+  }
+
+  onSubmit() {
+    const email = this.form.controls.email.value;
+    const password = this.form.controls.password.value;
+    const repeatPassword = this.form.controls.confirmPassword.value;
+    console.log(email, password, repeatPassword);
+  }
 
 }
