ffe01b8cb3fa7fb4f6bfa826177625458cc89ffd
Vinicius Burci <vpburci@gmail.com>
2025-04-25 16:55:08 -0300

    added workout, create-workout and workout-list components

---
+++ src/app/workout/create-workout/create-workout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-workout',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-workout.component.html',
+  styleUrl: './create-workout.component.css'
+})
+export class CreateWorkoutComponent {
+
+}
---
+++ src/app/workout/workout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout',
+  standalone: true,
+  imports: [],
+  templateUrl: './workout.component.html',
+  styleUrl: './workout.component.css'
+})
+export class WorkoutComponent {
+
+}
