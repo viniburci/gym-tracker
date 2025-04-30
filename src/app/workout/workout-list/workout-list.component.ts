9eb46831618977c0d06cd1779b5fc8bf6f48417a
Vinicius Burci <vpburci@gmail.com>
2025-04-30 20:33:13 -0300

    initial workoutList component

--- src/app/workout/workout-list/workout-list.component.ts
+++ src/app/workout/workout-list/workout-list.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Workout } from '../workout.model';
+import { WorkoutService } from '../workout.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-workout-list',
   standalone: true,
-  imports: [],
+  imports: [ RouterLink],
   templateUrl: './workout-list.component.html',
   styleUrl: './workout-list.component.css'
 })
 export class WorkoutListComponent {
+  workouts: Workout[] = [];
+  loading = true;
+  errorMessage = '';
+
+  private workoutService = inject(WorkoutService);
+
+  ngOnInit(): void {
+    this.loadWorkouts();
+  }
+
+  loadWorkouts(): void {
+    this.workoutService.getUserWorkouts().subscribe({
+      next: (data) => {
+        this.workouts = data;
+        this.loading = false;
+      },
+      error: (error) => {
+        this.errorMessage = 'Erro ao carregar workouts!';
+        console.error(error);
+        this.loading = false;
+      },
+    });
+  }
+
+  deleteWorkout(workoutId: number): void {
+    this.workoutService.deleteWorkout(workoutId).subscribe(() => {
+      this.workouts = this.workouts.filter(w => w.id !== workoutId);
+    });
+  }
 
 }
--- src/app/workout/workout.model.ts
+++ src/app/workout/workout.model.ts
@@ -1,10 +1,12 @@
 export interface Workout {
+  id?: number;
   name: string;
   workoutExercises: WorkoutExercise[];
   userEmail?: string;
 }
 
 export interface WorkoutExercise {
+  id?: number;
   exercise: {id: number};
   sets: number;
   reps: number;
