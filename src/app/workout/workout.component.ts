4d19fa181572caf5a03245a4207ab5f66d407bcb
Vinicius Burci <vpburci@gmail.com>
2025-08-19 18:44:48 -0300

    added edit + delete button on workoutView, and a simple confirm window for deleting

--- src/app/workout/workout.component.ts
+++ src/app/workout/workout.component.ts
@@ -1,17 +1,18 @@
 import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { Workout } from './workout.model';
 import { WorkoutService } from './workout.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-workout',
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './workout.component.html',
     styleUrl: './workout.component.css'
 })
 export class WorkoutComponent implements OnInit {
 
   private workoutService = inject(WorkoutService);
+  private router = inject(Router);
   private route = inject(ActivatedRoute);
 
   workout = signal<Workout | null>(null);
@@ -25,6 +26,16 @@ export class WorkoutComponent implements OnInit {
           //ordena os exercicios de forma crescente conforme o valor da position
           workout.workoutExercises.sort((a, b) => a.position - b.position);
         }
+      });
+    }
+  }
+
+  deleteWorkout(workoutId: number | undefined) {
+    const confirmed = window.confirm('Tem certeza que deseja excluir este treino?');
+
+    if (confirmed && workoutId) {
+      this.workoutService.deleteWorkout(workoutId).subscribe(() => {
+        this.router.navigate(['/workouts']);
       });
     }
   }
