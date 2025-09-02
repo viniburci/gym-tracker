c83b364a4e35c89d076a07ccb93f65a5e8093304
Vinicius Burci <vpburci@gmail.com>
2025-09-02 13:20:27 -0300

    redirect to login if try to access profile while not logged in

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { User } from '../auth/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,9 +12,13 @@ import { User } from '../auth/user.model';
 export class UserProfileComponent implements OnInit {
 
   user = signal<User | null>(null);
+  private router = inject(Router);
 
   ngOnInit() {
     this.user.set(localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')!) : null)
+    if(!this.user()) {
+      this.router.navigate(['/login']);
+    }
   }
 
 
